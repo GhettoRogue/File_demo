b4ce17e8fc40c402787a2c12648d0a6cc9b630f7
Vladimir Vladimirovich <125745422+GhettoRogue@users.noreply.github.com>
2023-04-11 17:04:31 +0400

    Add files via upload

---
+++ File_demo/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+    string path = "output.dat";
+
+    // record file
+    ofstream output;
+    output.open(path, ios::app);
+
+    if (!output.is_open()) {
+        cerr << "Error! File not opened" << endl;
+        return 1;
+    }
+
+    output << "Test text" << endl;
+
+    output.close();
+
+    // read file
+    ifstream input;
+    input.open(path);
+
+    if (!input.is_open()) {
+        cerr << "Error! File not opened" << endl;
+        return 1;
+    }
+
+    string line;
+    string text = "";
+    while (getline(input, line)) {
+        text += line + "\n";
+    }
+    cout << text << endl;
+
+    input.close();
+
+    return 0;
+}
